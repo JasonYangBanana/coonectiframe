d5b9337f7f1097912e8174ae93312ba8ad8150f0
JasonYang <jason19901207@gmail.com>
2018-11-15 02:34:20 +0800

    修改小window

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
         // console.log(destination.document.querySelector('receive-message'));
         console.log(e);
         console.log(e.data);
-        let receiveMessage = destination.document.querySelector('.receive-message')
+        let receiveMessage = document.querySelector('.receive-message')
         receiveMessage.textContent = e.data;
     }, false);
 })();
