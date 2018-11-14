e1c8e03b407f6471dcd712f9d77df81e48333325
JasonYang <jason19901207@gmail.com>
2018-11-14 22:58:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+//取得textarea的改變
+(function () {
+    let source = document.querySelector('#source');
+    let destination = document.getElementById('destination').contentWindow;
+    function getValue() {
+        let data = this.value;
+        destination.postMessage(data, '*');
+    }
+    source.addEventListener('input', getValue);
+})();
+
+let eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+let eventer = window[eventMethod];
+let messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+
+// Listen to message from child window
+eventer(messageEvent, function (e) {
+    // e.origin , check the message origin
+    console.log('parent received message!:  ', e.data);
+}, false);
